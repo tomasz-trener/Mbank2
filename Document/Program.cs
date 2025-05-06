63e1ae42a91c03de1e13c8a943ba2d681c97d206
Tomasz Leś <tomasz.les@pw.edu.pl>
2025-05-06 15:22:01 +0200

    q

---
+++ Document/Program.cs
@@ -0,0 +1,48 @@
+﻿using Azure;
+using Azure.AI.FormRecognizer.DocumentAnalysis;
+using System.Net;
+
+namespace P06DocumentIntelligence
+{
+    // Document reading model
+    internal class Program1
+    {
+        static async Task Main(string[] args)
+        {
+            string key = "";
+            string endpoint = "https://document01012.cognitiveservices.azure.com/";
+
+            DocumentAnalysisClient client = new DocumentAnalysisClient(new Uri(endpoint), new AzureKeyCredential(key));
+
+             Uri documentUri = new Uri("https://tomaszles.pl/pliki/pdf_sample.pdf");
+
+            AnalyzeDocumentOperation operation = await client.AnalyzeDocumentFromUriAsync(WaitUntil.Completed, "prebuilt-read",
+            documentUri);
+
+            AnalyzeResult result = operation.Value;
+
+            foreach (DocumentPage page in result.Pages)
+            {
+                Console.WriteLine($"Document Page {page.PageNumber} has {page.Lines.Count} lines and {page.Words.Count} words");
+
+                for (int i = 0; i < page.Lines.Count; i++)
+                {
+                    DocumentLine line = page.Lines[i];
+                    Console.WriteLine($"Line {i + 1}: {line.Content}");
+                }
+
+                for (int i = 0; i < page.Words.Count; i++)
+                {
+                    DocumentWord word = page.Words[i];
+                    Console.WriteLine($"Word {i + 1}: {word.Content} ({word.Confidence})");
+                }
+
+            }
+
+            foreach (DocumentParagraph paragraph in result.Paragraphs)
+            {
+                Console.WriteLine($"{paragraph.Role}: {paragraph.Content}");
+            }
+        }
+    }
+}
